316bc37b8cb72274db9f2a84fc18834f56a0b0eb
yaswanth kandipati <yaswanth.kandipati@gmail.com>
2020-08-03 17:03:09 +0530

    Ex1

---
+++ Milestone-2/src/com/wipro/threadcontrolandpriorities/Ex1.java
@@ -0,0 +1,22 @@
+package com.wipro.threadcontrolandpriorities;
+
+public class Ex1 implements Runnable {
+	static Thread t1;
+
+	public static void main(String[] args) {
+	t1 = new Thread(new Ex1());
+	t1.start();
+	}
+	@Override
+	public void run() {
+	for (int i = 1; i <= 10; i++) {
+	if (i == 6)
+	try {
+	t1.sleep(5000);
+	} catch (InterruptedException e) {
+	e.printStackTrace();
+	}
+	System.out.println(i);
+	}
+	}
+}
